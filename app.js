8d3bd8bd1003e6df20bc943e097304481d885b03
TheArthurVincent <arthurcardosocorp@gmail.com>
2023-09-08 09:04:38 -0300

    cors fixed

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const app = express();
 const connection = require("./db/conn");
 const PORT = 3501;
 
-app.use(cors());
+let isDev = true;
+// isDev = false;
+const front = isDev ? "http://localhost:5173" : "";
+
+app.use(
+  cors({
+    origin: front,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 connection();
---
+++ server/constants/constants.js
@@ -0,0 +1,3 @@
+let isDev = true;
+// isDev = false;
+export const front = isDev ? "http://localhost:5173" : "";
