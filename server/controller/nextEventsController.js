8fe25e7c102ab0b4c3cd5b5283201c4b3464c1ca
TheArthurVincent <arthurcardosocorp@gmail.com>
2024-01-04 18:20:21 -0300

    *

--- server/controller/nextEventsController.js
+++ server/controller/nextEventsController.js
@@ -54,7 +54,6 @@ const nextTutoring_editNext = async (req, res) => {
           `SUCESSO - E-mail enviado e aula particular de ${student.name}- ${formattedDate} às ${time} marcada - ${meetingUrl}`,
           "text/html"
         );
-        console.log(`E-mails de aula marcada enviados ${new Date()}`);
       } catch (e) {
         sendEmail(
           "arvinenglishschool@gmail.com",
