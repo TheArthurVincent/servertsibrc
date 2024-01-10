f0f2a5a5442e0e8cf1542f0b70ee6226d61f72c9
Rafael <rafaelfreire0@gmail.com>
2024-01-10 18:51:20 -0300

    Feat: Sendpulse para envio de emails
    implementado sdk da send pulse para envio de emails e sms

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,7 +1,17 @@
 const Student_Model = require("../models/Students");
+//importe o arquivo sendpulse.js
+const mailSend = require("../useful/sendpulse");
+
+//crie uma instancia da classe mailSend
+const mail = new mailSend();
 
 const signup = async (req, res, next) => {
   const newUser = await Student_Model.create(req.body);
+
+  const {email, name} = req.body;
+
+  //aqui é onde envia o email, o corpo do email deve ser uma string de html (ou um ejs carregado)
+  await mail.sendMail(email, name, "Bem vindo ao Arvin", "<h1>Seja bem vindo!</h1>");
 
   res.status(201).json({
     status: "success",
---
+++ server/useful/sendpulse.js
@@ -0,0 +1,63 @@
+const sendpulse = require("sendpulse-api");
+
+let API_USER_ID, API_SECRET, TOKEN_STORAGE;
+
+//aqui deve subistituir com suas credenciais
+API_USER_ID = "b8faa40b161548cf666b5a2a275cb78b";
+API_SECRET = "cb960b298d6730d324f168455fe58fce";
+//essa sera uma pasta temporaria para armazenar o token
+TOKEN_STORAGE = "/tmp/";
+
+class mailSend {
+
+  //função para pegar a resposta do servidor de email
+  async answerGetter(data) {
+    console.log(data);
+  }
+
+  //função para enviar o email
+  async sendMail(tomail, name, subject, body) {
+
+    let answerGetter = this.answerGetter;
+
+    //aqui é o corpo do email
+    let emailData = {
+      html: body,
+      subject: subject,
+      from: {
+        name: "Contato", //aqui é o nome que vai aparecer no email
+        email: "contato@digitalmoontech.com.br" //aqui é o email que vai aparecer no email (deve ser o mesmo configurado como remetente*)
+      },
+      to: [
+        {
+          name: name,
+          email: tomail
+        }
+      ]
+    }
+
+    return sendpulse.init(API_USER_ID, API_SECRET, TOKEN_STORAGE, (token) => {
+      console.log(token);
+
+      sendpulse.smtpSendMail(answerGetter, emailData);
+    });
+
+
+
+  }
+
+  //função para enviar o sms (ainda não testado)
+  async sendSMS(phone, text) {
+
+    let answerGetter = this.answerGetter;
+
+    return sendpulse.init(API_USER_ID, API_SECRET, TOKEN_STORAGE, (token) => {
+      console.log(token);
+
+      sendpulse.smsSend(answerGetter, "Digitalmoon", [phone], text);
+    });
+
+  }
+}
+
+module.exports = mailSend;
