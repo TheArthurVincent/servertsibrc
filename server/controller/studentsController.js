e375766ff15bc9dca6d47aecbe989d7561a78bed
TheArthurVincent <arthurcardosocorp@gmail.com>
2024-03-14 21:43:54 -0300

    *

--- server/controller/studentsController.js
+++ server/controller/studentsController.js
@@ -47,7 +47,7 @@ const students_getTotalAllScores = async (req, res) => {
         student._id.toString() !== "651311fac3d58753aa9281c5" &&
         student._id.toString() !== "658c9349adb27531cae962d3" &&
         student._id.toString() !== "6586ed9f7c72f31329eca797" &&
-        student.totalScore !== 0
+        student.totalScore > 0
     );
 
     const formattedStudentsData = filteredStudents.map((student, index) => {
